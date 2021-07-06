d9c4a0627f933b87e9e01581f025121a82850c09
bang88 <sqibang@gmail.com>
2021-07-06 15:43:05 +0800

    chore: to be continued

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,7 +1,6 @@
 // @flow strict
 import React from 'react';
 import { Link } from 'gatsby';
-import Author from './Author';
 import Comments from './Comments';
 import Content from './Content';
 import Meta from './Meta';
