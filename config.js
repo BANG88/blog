6b13c0075e38b4b2f489275e95e9e2de1f4e0ff7
bang88 <sqibang@gmail.com>
2021-07-06 09:47:44 +0800

    initial

---
+++ config.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = {
+  url: 'https://domybest.dev',
+  pathPrefix: '/',
+  title: 'JUST DO IT.',
+  subtitle: 'Nothing.',
+  copyright: '© ❤',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: 'UA-80533315-1',
+  useKatex: false,
+  menu: [
+    {
+      label: 'Notes',
+      path: '/'
+    },
+    {
+      label: 'About',
+      path: '/pages/about'
+    }
+  ],
+  author: {
+    name: '阿邦',
+    photo: '/avatar.jpg',
+    bio: 'Life is too short for the wrong job.',
+    contacts: {
+      email: '',
+      facebook: '',
+      telegram: '',
+      twitter: '#',
+      github: 'bang88',
+      rss: '',
+      vkontakte: '',
+      linkedin: '',
+      instagram: '',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: '',
+      youtube: '',
+      soundcloud: '',
+      medium: '',
+    }
+  }
+};
